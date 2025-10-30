638f89a2043478f46743a9e3110d54d71c235a55
Maaz Malik <130928892+MaazMalikSecurity@users.noreply.github.com>
2025-10-30 17:40:19 +0500

    Change response message for root route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Deployment Successful');
+    res.send('Hello World from AWS CI/CD');
 });
 
 app.listen(port, () => {
